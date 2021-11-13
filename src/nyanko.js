99fb2f538d0d796850f6c6bc63be3946049783ab
jackesque <92624346+Jackesque@users.noreply.github.com>
2021-11-13 11:53:07 +0700

    Update and rename robots.js to nyanko.js

--- src/nyanko.js
+++ src/nyanko.js
@@ -1,62 +1,74 @@
 export const robots = [
   {
+    id: 0,
+    name: 'Jackesque Cristant',
+    username: 'Jac_Cris',
+    email: 'jac.cris@coolblaze.com'
+  },
+  {
     id: 1,
     name: 'Leanne Graham',
     username: 'Bret',
-    email: 'Sincere@april.biz'
+    email: 'sincere@april.biz'
   },
   {
     id: 2,
     name: 'Ervin Howell',
     username: 'Antonette',
-    email: 'Shanna@melissa.tv'
+    email: 'shanna@melissa.tv'
   },
   {
     id: 3,
     name: 'Clementine Bauch',
     username: 'Samantha',
-    email: 'Nathan@yesenia.net'
+    email: 'nathan@yesenia.net'
   },
   {
     id: 4,
     name: 'Patricia Lebsack',
     username: 'Karianne',
-    email: 'Julianne.OConner@kory.org'
+    email: 'julianne.OConner@kory.org'
   },
   {
     id: 5,
     name: 'Chelsey Dietrich',
     username: 'Kamren',
-    email: 'Lucio_Hettinger@annie.ca'
+    email: 'lucio_Hettinger@annie.ca'
   },
   {
     id: 6,
     name: 'Mrs. Dennis Schulist',
     username: 'Leopoldo_Corkery',
-    email: 'Karley_Dach@jasper.info'
+    email: 'karley_Dach@jasper.info'
   },
   {
     id: 7,
     name: 'Kurtis Weissnat',
     username: 'Elwyn.Skiles',
-    email: 'Telly.Hoeger@billy.biz'
+    email: 'telly.Hoeger@billy.biz'
   },
   {
     id: 8,
     name: 'Nicholas Runolfsdottir V',
     username: 'Maxime_Nienow',
-    email: 'Sherwood@rosamond.me'
+    email: 'sherwood@rosamond.me'
   },
   {
     id: 9,
     name: 'Glenna Reichert',
     username: 'Delphine',
-    email: 'Chaim_McDermott@dana.io'
+    email: 'chaim_McDermott@dana.io'
   },
   {
     id: 10,
     name: 'Clementina DuBuque',
     username: 'Moriah.Stanton',
-    email: 'Rey.Padberg@karina.biz'
+    email: 'rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Alice von April',
+    username: 'Alice',
+    email: 'AvA.royal@german.army'
   }
-];
+]
